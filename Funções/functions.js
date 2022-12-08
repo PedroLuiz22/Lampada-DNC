334d06b7f4dc01dbfd31b74485e28590088cda8e
Pedro Luiz <pedrolluiz@hotmail.com>
2022-12-08 10:44:03 -0300

    Pastas removidas

--- Funções/functions.js
+++
@@ -1,9 +0,0 @@
-function presente (dinheiro, valor){
-    if(dinheiro < valor){
-        return 'Não vai dar para comprar'
-    }else {
-        return 'Você precisa fazer a compra?'
-    }
-}
-let x = presente(100,90)
-console.log (x)
--- Repetições/repeticoes02.js
+++
@@ -1,5 +0,0 @@
-const bolsa = ['maquiagem','escova de cabelo','espelho','carteira','celular', 'carregador']
-for (let i = 0; i < bolsa.length ; i++) {
-    console.log(bolsa [i])
-    if (bolsa[i] == 'celular') break
-}
